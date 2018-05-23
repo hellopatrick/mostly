ab7b1aac1d9846972eff477714d062a9df77c0ff
Patrick Rogers <patrick@helloresolven.com>
2018-05-23 00:06:55 -0700

    feat(index): add set intersection of two sets of artists.

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ require("dotenv").config();
 
 const Future = require("fluture");
 
-const { relatedArtist } = require("./src/spotify");
+const { relatedArtists } = require("./src/spotify");
 
 const argv = Future.attempt(() => process.argv);
 const names = argv.map(args => args.slice(2));
 
-const related = relatedArtist;
+const commonRelatedArtists = artists => others =>
+  artists.filter(artist => others.includes(artist));
 
-const similar = one => two => [one, two];
-
-const main = ([one, two]) => {
-  return Future.of(similar)
-    .ap(related(one))
-    .ap(related(two));
+const main = ([fst, snd]) => {
+  return Future.of(commonRelatedArtists)
+    .ap(relatedArtists(fst))
+    .ap(relatedArtists(snd));
 };
 
 names.chain(main).fork(console.error, console.log);
