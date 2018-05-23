b07894e9ce39549da2b123e3099c54f4103d9c1b
Patrick Rogers <patrick@helloresolven.com>
2018-05-23 08:12:52 -0700

    feat(spotify): use get prop to get data as well.

--- src/spotify.js
+++ src/spotify.js
@@ -7,15 +7,13 @@ const { getProp, resultToFuture, map } = require("./utils");
 const findArtist = name =>
   client
     .chain(get => get(`/search?q=${name}&type=artist`))
-    .map(res => res.data)
-    .map(getProp("artists", "items", 0, "id"))
+    .map(getProp("data", "artists", "items", 0, "id"))
     .chain(resultToFuture);
 
 const related = id =>
   client
     .chain(get => get(`/artists/${id}/related-artists`))
-    .map(res => res.data)
-    .map(getProp("artists"))
+    .map(getProp("data", "artists"))
     .chain(resultToFuture)
     .map(artists => artists.map(({ name }) => name));
 
